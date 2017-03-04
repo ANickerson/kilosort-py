6f2ea603a8ca3fb67ec9defcc9f50e718a7887d2
Aidan <firecraker180@gmail.com>
2017-03-04 22:06:40 +0000

    cleanup of code

---
+++ src/config.py
@@ -0,0 +1,51 @@
+"""
+Config class for kilsort.
+
+
+"""
+
+
+class Config:
+    def __init__(self):
+        """The configuration class for kilsort.
+        @TODO: convert this class into a dictionary.
+        """
+        self.gpu = True  #This is redundant as it will be handled by theano
+        self.parfor = False  #Not sure if this is valid
+        self.verbose = True
+        self.showfigures = True
+        self.datatype = 'dat'  #not sure if it makes sense to give the option for openephys
+
+        self.n_filt = 64  #number of clusters to use
+        self.whitening_range = 32  #number of channels to whiten together (-1 for all)
+
+        self.n_rank = 3  #matrix rank of spike template model
+        self.nfullpasses = 6  # number of complete passes during optimization
+        self.max_fr = 20000  # max no. of spikes to extract per batch
+
+        self.fshigh = 300  # frequency for high pass filtering (Hz)
+        self.fslow = 2000  # frequency for low pas filtering (Hz)
+        self.ntbuff = 64  # samples of symetrical buffer for whitening and spike detection
+        self.scaleproc = 200  #int16 scaling of whitened data :TODO What???
+        self.batch_size = 32 * 1024 + self.ntbuff  #batch size
+
+        self.spike_threshold = [
+            4, 10, 10
+        ]  # threshold for detecting spikes on template-filtered data
+        self.large_means_amplitudes = [
+            5, 20, 20
+        ]  # large means amplitudes are foced around the mean :TODO what?
+        self.n_anneal_passes = 4  # should be less than fullpasses :TODO what?
+        self.shuffle_clusters = 1  # allow merges and splits during optimisation
+        self.merge_threshold = 0.1  # upper threshold for merging
+        self.split_threshold = 0.1  # lower threshold for splitting
+
+
+class Signal:
+    def __init__(self, data, fs, coordinatemap, n_channel):
+        """A basic class for a signal from a single recording site
+        """
+        self.data = data
+        self.framerate = fs
+        self.coords = coordinatemap
+        self.n_channel = n_channel  # total number of channels
--- src/preprocessing.py
+++ src/preprocessing.py
@@ -8,11 +8,15 @@ butterworth filter
 whitening
 
 """
-from scipy import signal
+
 import numpy as np
+import theano
+from theano import tensor as T
+from theano.tensor import nlinalg
+from scipy import signal
 
 
-def batchProcess(data, batch_size=1000, parallel=False):
+def batch_process(data, batch_size=1000, parallel=False):
     """
     Proces a signal by batch
 
@@ -20,64 +24,71 @@ def batchProcess(data, batch_size=1000, parallel=False):
     :param batch_size: size of the signal to run
     :param parallel: execute in parallel @TODO: implement parallel batch processing
     """
+    pass
 
-    
-def preprocessData(sig, filtertype="butter", lfreq=300, hfreq=20000, whiten=True):
+
+def preprocess_data(sig,
+                    filtertype="butter",
+                    lfreq=300,
+                    hfreq=20000,
+                    whiten=True):
     """
     run the process steps on the given input data segment.
     
     :type sig: config.signal
     """
-   
-   signalarr = sig.data 
-   #highpass filter at 300Hz
-   a, b = signal.butter(3, (lfreq * 2) / (sig.fs), "highpass")
-   signalarr = signal.filtfilt(data,a,b)
 
-   # common average reference
-   signalarr -= np.median(signalarr,0)[:,np.newaxis]
-   
+    signalarr = sig.data
+    #highpass filter at 300Hz
+    a, b = signal.butter(3, (lfreq * 2) / (sig.fs), "highpass")
+    signalarr = signal.filtfilt(signalarr, a, b) #@TODO: implement filter in theano?
 
-   #whiten data to remove correlated noise
-   #and we therefore compute the columns of the whitening matrix W ZCA  independently for each channel,  based on its nearest 32 neighbors. @TODO: sort this out!
-   signalarr = ZCAWhiten(signalarr)
+    # common average reference
+    signalarr -= np.median(signalarr, 0)[:, np.newaxis]
+
+    #whiten data to remove correlated noise
+    #and we therefore compute the columns of the whitening matrix W ZCA
+    # independently for each channel,  based on its nearest 32 neighbors.
+    # @TODO: sort this out!
+    signalarr = zca_whiten(signalarr)
 
 
-def ZCAWhiten(data):
-    m_ = np.mean(data, 0)
-    data_ = data - m
-    cov = np.dot(data_.T, data_) / (data_.shape[0]-1)
-    U, S, _ = np.linalg.svd(cov)
-    s = np.sqrt(S)#S.clip(self.regularization))
-    s_inv = np.diag(1./s)
+def zca_whiten(data):
+    """zca whitening method for data."""
+    m = np.mean(data, 0) # take mean
+    _data = data - m # demean data
+    cov = np.dot(_data.T, _data) / (_data.shape[0] - 1) #dot product of data.T, data devide by len-1
+    U, S, _ = np.linalg.svd(cov) # svd of covariance matrix
+    s = np.sqrt(S)  #S.clip(self.regularization)) 
+    s_inv = np.diag(1. / s)
     s = np.diag(s)
-    whiten_ = np.dot(np.dot(U, s_inv), U.T)
-    return np.dot(data_, whiten_.T)
+    _whiten = np.dot(np.dot(U, s_inv), U.T)
+    return np.dot(_data, _whiten.T)
     #self.dewhiten_ = np.dot(np.dot(U, s), U.T)
 
 
-from theano import tensor as T
-import theano
-from theano.tensor import nlinalg
+class TheanoFunctions:
+    def __init__(self):
+        """cached theano functions. Probably not the best way to do this."""
+        self._ZCAWhiten = None
 
-class theanoFunctions:
-    def __init__():
-        pass
 
-    def ZCAWhiten(data):
+    def zca_whiten(self, data):
+        """
+        zca whitening method for data.
+        """
         if self._ZCAWhiten is None:
 
-            x = T.dmatrix('x', theano.floatX)
+            x = T.dmatrix('x', T.floatX)
             m_ = x.mean(0)
             data_ = x - m_
             cov = theano.dot(x.T, x) / (x.shape[0] - 1)
             U, S, _ = nlinalg.svd(cov)
-            s = T.sqrt(S)
-            s_inv = T.diag(1./s)
+            s = S.sqrt()
+            s_inv = T.diag(1. / s)
             s = T.diag(s)
-            whiten_ = T.dot(T.dot(U, s_inv), U.T)
-            out = T.dot(data_, whiten_.T)
+            whiten_ = U.dot(s_inv).dot(U.T)
+            out = data_.dot(whiten_.T)
 
-            self._ZCAWhiten = theano.function([x],out)
+            self._ZCAWhiten = theano.function([x], out)
         return self._ZCAWhiten(data)
-
