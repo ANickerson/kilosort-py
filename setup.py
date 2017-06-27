7e2751a1cab100b418f32765043a4f1bb159fc29
Aidan <firecraker180@gmail.com>
2017-06-27 21:55:50 +0100

    added name to setup

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 setup(
-    name="HelloWorld",
+    name="kilosort-py",
     version="0.0",
     packages=find_packages(),
     install_requires=['numpy>1.12']
-    )
+)
